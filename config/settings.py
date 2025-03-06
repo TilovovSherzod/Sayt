246d5231649aa10cfbd9f8b2f3157335e9b22f67
TilovovSherzod <168565454+TilovovSherzod@users.noreply.github.com>
2025-03-06 23:58:01 +0500

    Update settings.py
    tayyor

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'ShohMarket_db'),
         'USER': os.getenv('DB_USER', 'ShohMarket_admin'),
         'PASSWORD': os.getenv('DB_PASSWORD', '1234'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
