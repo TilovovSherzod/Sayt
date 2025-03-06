bd4baddfc7cb9fadef7057aae48006c82aef2e6e
TilovovSherzod <168565454+TilovovSherzod@users.noreply.github.com>
2025-03-06 23:56:17 +0500

    Python Djangoda yozilgan savdo veb sayti !!!

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME', 'ShohMarket_db'),
+        'USER': os.getenv('DB_USER', 'ShohMarket_admin'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '1234'),
+        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
